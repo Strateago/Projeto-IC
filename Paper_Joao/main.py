c7fa0778d220bdd0904e4a7986977c7b8ae1cd21
Thiago <thiagojgcosta@gmail.com>
2026-06-25 16:45:03 -0300

    update main

--- Paper_Joao/main.py
+++ Paper_Joao/main.py
@@ -6,6 +6,7 @@ import os
 import time
 import ilupp
 from load_data import load_data
+import matplotlib.pyplot as plt
 
 threshold = 1e-6
 fill = 2 # ILU1
@@ -13,19 +14,94 @@ tol = 1e-4
 restart = 10 
 
 data_path = './Projeto-IC/Paper_Joao/dados'
-# data = []
-# for dir in os.listdir(f'{data_path}'):
-#     data.append(load_data(f'{data_path}/{dir}'))
-#     ## continuar em breve, depois de tentar com um único
 
-problem = "layer_1_Cr30"
+problem = "layer_85_Cr30"
 data = load_data(f'{data_path}/{problem}')
 
-M = ilupp.ILUTPreconditioner(data['T'], fill_in=fill, threshold=threshold)
-# M = ilupp.ILU0Preconditioner(data['T'])
-residuals = []
-sol, code = fgmres(data['T'], data['b'], M=M, tol=tol, residuals=residuals, restart=restart)
+## Resultado fina
 
-print(f"Code: {code}")
-print(f"Residuals: {residuals}")
-print(f"Solution: {sol}")
+# M = ilupp.ILUTPreconditioner(data['T'], fill_in=fill, threshold=threshold)
+# # M = ilupp.ILU0Preconditioner(data['T'])
+# residuals_ref = []
+# sol_ref, code_ref = fgmres(data['T'], data['b'], M=M, tol=tol, residuals=residuals_ref, restart=restart)
+sol_ref = spla.spsolve(data['T'], data['b'])
+
+## Resultado Multiescala
+
+plt.figure()
+plt.xlabel("Iterações")
+plt.ylabel("Resíduo")
+plt.title(f'Resíduos: Camada 85 - ILU0')
+plt.yscale("log")
+plt.grid(True)
+problems = []
+
+for dir in [d for d in os.listdir(f'{data_path}') if d.startswith('layer_85')]:
+    print(dir)
+    data = load_data(f'{data_path}/{dir}')
+
+    Ac = data['OR'] @ data['T'] @ data['OP']
+    b_c = data['OR'] @ data['b']
+
+    # M = ilupp.ILUTPreconditioner(Ac, fill_in=fill, threshold=threshold)
+    # # M = ilupp.ILU0Preconditioner(data['T'])
+    # residuals_ms = []
+    # sol, code = fgmres(Ac, b_c, M=M, tol=tol, residuals=residuals_ms, restart=restart)
+    # sol = spla.spsolve(Ac, b_c)
+
+    # sol_f = data['OP'] @ sol
+
+    # # print(f"Solutions:\n   ref: {sol_ref}\n   ms: {sol_f}")
+
+    # erro_absoluto = np.linalg.norm(sol_ref - sol_f)
+    # erro_relativo = erro_absoluto / np.linalg.norm(sol_ref)
+
+    # print(f"Erro Relativo: {erro_relativo:.4f} ({erro_relativo * 100:.2f}%)")
+
+
+    init = time.time()
+
+    linOP = ilupp.ILU0Preconditioner(Ac)
+    residuals = []
+    def callback(xk):
+        r = b_c - Ac @ xk
+        residuals.append(np.linalg.norm(r))
+
+    # Solve the system with GMRES
+    x, info = spla.gmres(
+        Ac,
+        b_c,
+        x0=None,
+        M=linOP,
+        rtol=tol,
+        maxiter=100,
+        callback=callback,
+        callback_type="x"
+    )
+
+    end = time.time()
+
+    # print(info)
+    sol_f = data['OP'] @ x
+
+    erro_absoluto = np.linalg.norm(sol_ref - sol_f)
+    erro_relativo = erro_absoluto / np.linalg.norm(sol_ref)
+
+    # print(f"Erro Relativo (gmres): {erro_relativo:.4f} ({erro_relativo * 100:.2f}%)\n")
+
+    # print(residuals, '\n')
+
+    plt.plot(residuals, label=dir)
+    problems.append(dir)
+
+
+plt.legend(problems, loc='center left', bbox_to_anchor=(1.02, 0.5), borderaxespad=0.)
+plt.tight_layout()
+plt.show()
+plt.close()
+
+
+# print(f"Code (Reference): {code_ref}")
+# print(f"Code (Multiscale): {code}")
+# print(f"Residuals (Reference): {residuals_ref}")
+# print(f"Residuals (Multiscale): {residuals_ms}")
